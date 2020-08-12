23083be4bb7bd6ec6c667048bda4f22eb1eebd88
Samuel Santos <samuellima280499@gmail.com>
2020-08-12 02:55:39 -0300

    Update Day 2

---
+++ server/src/routes.ts
@@ -0,0 +1,15 @@
+import { Router } from 'express';
+import PointsController from './controllers/PointsController';
+import ItemsController from './controllers/ItemsController';
+
+const pointsController = new PointsController();
+const itemsController = new ItemsController();
+
+const routes = Router();
+
+routes.get('/items', itemsController.index);
+routes.post('/points', pointsController.create);
+routes.get('/points', pointsController.index);
+routes.get('/points/:id', pointsController.show);
+
+export default routes;
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,15 @@
-import express from "express";
+import express from 'express';
+import routes from './routes';
+import path from 'path';
+import cors from 'cors';
 
 const app = express();
 
-app.get("/users", (req, res) => {
-  return res.json({ message: "Hello My friend" });
-});
+app.use(cors());
+// {origin: 'www.site.com.br'} o que fica no cors
+app.use(express.json());
+app.use(routes);
+
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 app.listen(3333);
